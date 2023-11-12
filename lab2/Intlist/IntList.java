7a4230949bbc695a160db48e16438a7a082aaf2a
DIVIN <2756978878@qq.com>
2023-11-12 14:52:01 +0800

    lab2.0

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,10 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        while (A != null)
+            A = A.rest;
+        A = B;
+        return A;
     }
 
     /**
@@ -91,7 +94,11 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList C = A;
+        while (C != null)
+            C = C.rest;
+        C = B;
+        return C;
     }
 
 
